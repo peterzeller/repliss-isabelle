c485d034169ee93cd3580f1ed78fdbab4605c0a7
Peter Zeller <p_zeller@cs.uni-kl.de>
2017-03-16 18:51:10 +0100

    todos

---
+++ approach.thy
@@ -0,0 +1,18 @@
+theory approach
+imports Main sem1_commutativity
+begin
+
+
+(*
+TODO:
+
+1. single session semantics
+
+2. show that multi-session trace with fixed session can be reduced to multiple single session traces by chopping
+
+3. other things
+
+*)
+
+
+end
