fd4b5224ad21b2ba1717abde82f9deef8e8a7d36
Peter Zeller <p_zeller@cs.uni-kl.de>
2017-07-05 12:43:06 +0200

    prefix theory

---
+++ prefix.thy
@@ -0,0 +1,23 @@
+theory prefix
+imports Main
+begin
+
+text {* Function describing that a list is a prefix of another list.  *}
+definition 
+"isPrefix xs ys \<equiv> xs = take (length xs) ys"
+
+lemma isPrefix_append:
+"isPrefix xs ys \<Longrightarrow> isPrefix xs (ys@zs)"
+using take_all by (fastforce simp add: isPrefix_def)
+
+lemma isPrefix_refl[simp]: "isPrefix xs xs"
+  by (simp add: isPrefix_def)
+
+lemma isPrefix_empty[simp]: "isPrefix xs [] \<longleftrightarrow> xs = []"
+  by (simp add: isPrefix_def)
+
+lemma isPrefix_appendI[simp]:
+"isPrefix xs (xs@ys)"
+  by (simp add: isPrefix_def)
+
+end
