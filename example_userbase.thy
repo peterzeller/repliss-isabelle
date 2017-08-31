0a478fc47322b5a4b8f9d919cbc3b673a36e4891
Peter Zeller <p_zeller@cs.uni-kl.de>
2017-08-31 18:44:50 +0200

    started with userbase example

---
+++ example_userbase.thy
@@ -0,0 +1,61 @@
+theory example_userbase
+imports approach
+begin
+
+datatype val =
+    String string
+  | UserId int
+  | Undef
+
+record localState = 
+  ls_pc :: nat
+  ls_u :: val
+  ls_name :: string
+  ls_mail :: string
+
+definition initialState :: "localState" where
+"initialState = \<lparr>
+  ls_pc = 0,
+  ls_u = Undef,
+  ls_name = '''',
+  ls_mail = ''''
+\<rparr>"
+  
+(*
+  querySpec :: "operation \<Rightarrow> 'any list \<Rightarrow> 'any operationContext \<Rightarrow> 'any \<Rightarrow> bool"
+  procedure :: "procedureName \<Rightarrow> 'any list \<rightharpoonup> ('localState \<times> ('localState, 'any) procedureImpl)"
+  invariant :: "'any invariantContext \<Rightarrow> bool"
+*)  
+
+definition registerUserImpl :: "(localState, val) procedureImpl" where
+"registerUserImpl ls \<equiv> [
+   (* 0 *) NewId (\<lambda>x. (ls\<lparr>ls_u := x, ls_pc := 1\<rparr> :: localState)),
+   (* 1 *) BeginAtomic (ls\<lparr>ls_pc := 2\<rparr>),
+   (* 2 *) DbOperation ''users_name_assign'' [ls_u ls, String (ls_name ls)] (\<lambda>r. ls\<lparr>ls_pc := 3\<rparr>),
+   (* 3 *) DbOperation ''users_mail_assign'' [ls_u ls, String (ls_mail ls)] (\<lambda>r. ls\<lparr>ls_pc := 4\<rparr>),
+   (* 4 *) EndAtomic (ls\<lparr>ls_pc := 5\<rparr>),
+   (* 5 *) Return (ls_u ls)
+   ] ! ls_pc ls"
+
+definition procedures where
+"procedures proc args \<equiv> 
+  if proc = ''registerUser'' then
+    case args of 
+      [String name, String mail] \<Rightarrow> 
+        Some (initialState\<lparr>ls_name := name, ls_mail := mail \<rparr> , registerUserImpl)
+      | _ \<Rightarrow> None
+  else 
+    None"
+
+    
+    
+definition prog :: "(localState, val) prog" where
+"prog \<equiv> \<lparr>
+  querySpec = ???,
+  procedure = procedures,
+  invariant = ???
+\<rparr>"
+
+
+
+end
