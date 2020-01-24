0485c1a68f3d3fe46f1c84bb7d9a9397b079c2f6
Peter Zeller <p_zeller@cs.uni-kl.de>
2020-01-24 15:44:00 +0100

    assertions completeness

--- completeness.thy
+++ completeness.thy
@@ -7,33 +7,47 @@ begin
 
 lemma false_step_invarariant_false:
   assumes "S ~~ (i, a, False) \<leadsto>\<^sub>S S'"
-  shows "\<not>invariant_all S'"
+  shows "\<not>invariant_all S' \<or> a = ALocal False"
   using assms by (auto simp add: step_s.simps)
 
 lemma completeness_1:
   assumes step_s: "S ~~ (i, a, False) \<leadsto>\<^sub>S S'"
     and wf: "state_wellFormed S"
-  shows "\<exists>tr. initialState (prog S) ~~ tr \<leadsto>* S'" 
+  shows "\<exists>tr. (initialState (prog S) ~~ tr \<leadsto>* S') \<and> (i,a) \<in> set tr" 
 proof (cases a)
 next
   case AEndAtomic
   with step_s
   have "S ~~ (i, a) \<leadsto> S'"
     by (auto simp add: step_s.simps step.simps)
+  obtain tr where "initialState (prog S) ~~ tr \<leadsto>* S"
+    using local.wf state_wellFormed_def by blast
+
+  with `S ~~ (i, a) \<leadsto> S'`
+  have "initialState (prog S) ~~ tr@[(i,a)] \<leadsto>* S'"
+    using wf state_wellFormed_def steps_step by blast
+
   thus ?thesis
-    using wf state_wellFormed_def steps_step by blast 
+    by auto
 next
   case (AReturn r)
   with step_s
   have "S ~~ (i, a) \<leadsto> S'"
     by (auto simp add: step_s.simps step.simps)
+  obtain tr where "initialState (prog S) ~~ tr \<leadsto>* S"
+    using local.wf state_wellFormed_def by blast
+
+  with `S ~~ (i, a) \<leadsto> S'`
+  have "initialState (prog S) ~~ tr@[(i,a)] \<leadsto>* S'"
+    using wf state_wellFormed_def steps_step by blast
+
   thus ?thesis
-    using wf state_wellFormed_def steps_step by blast 
+    by auto
 next
   case (AInvoc p)
   show ?thesis
   proof (rule ccontr)
-    assume "\<nexists>tr. initialState (prog S) ~~ tr \<leadsto>* S'"
+    assume "\<nexists>tr. (initialState (prog S) ~~ tr \<leadsto>* S') \<and> (i, a) \<in> set tr "
 
     from AInvoc step_s
     show False
@@ -51,15 +65,35 @@ next
         by (auto simp add: step.simps  InvocStep)
 
       from `state_wellFormed S_invoc`
-      obtain S_init tr1 where "S_init ~~ tr1 \<leadsto>* S_invoc"
-        using state_wellFormed_def by blast
+      obtain tr1 where "initialState (prog S) ~~ tr1 \<leadsto>* S_invoc"
+        using state_wellFormed_def ` prog S_invoc = prog S` by metis
 
-
-      then show ?case
-        by (metis InvocStep(10) \<open>S_invoc ~~ (i, a) \<leadsto> S'\<close> \<open>\<nexists>tr. initialState (prog S) ~~ tr \<leadsto>* S'\<close> local.wf prog_inv state_wellFormed_combine_s1 state_wellFormed_def step_s)
+      with `S_invoc ~~ (i,a) \<leadsto> S'`
+      show ?case
+        using \<open>\<nexists>tr. (initialState (prog S) ~~ tr \<leadsto>* S') \<and> (i, a) \<in> set tr\<close> steps_step by force
     qed
   qed
+next
+  case (ALocal ok)
 
+  from ALocal `S ~~ (i, a, False) \<leadsto>\<^sub>S S'`
+  have "ok = False"
+    by (auto simp add: step_s.simps)
+
+  from ALocal `S ~~ (i, a, False) \<leadsto>\<^sub>S S'` 
+  have a: "S ~~ (i, a) \<leadsto> S'"
+    by (auto simp add: step_s.simps step.simps)
+
+  obtain tr where b: "initialState (prog S) ~~ tr \<leadsto>* S"
+    using local.wf state_wellFormed_def by blast
+
+  with `S ~~ (i, a) \<leadsto> S'`
+  have "initialState (prog S) ~~ tr@[(i, a)] \<leadsto>* S'"
+    using steps_step by blast
+
+
+  thus "?thesis"
+    by auto
 qed (insert assms, auto simp add: step_s.simps)
 
 
@@ -97,26 +131,39 @@ proof (rule ccontr)
 
 
   from this obtain tr
-    where "initialState program ~~ tr \<leadsto>* S'"
+    where "initialState program ~~ tr \<leadsto>* S'" and "(i,a) \<in> set tr"
     using \<open>S ~~ (i, a, False) \<leadsto>\<^sub>S S'\<close> \<open>state_wellFormed S\<close> completeness_1 by blast
 
-  have "\<not>invariant_all S'"
+  have "\<not>invariant_all S' \<or> a = ALocal False"
     using \<open>S ~~ (i, a, False) \<leadsto>\<^sub>S S'\<close> false_step_invarariant_false by blast
 
+  thus False
+  proof
+    assume "\<not>invariant_all S'"
 
-  hence "S' ~~ (i, AInvcheck False) \<leadsto> S'"
-    by (auto simp add: step.simps)
+    hence "S' ~~ (i, AInvcheck False) \<leadsto> S'" 
+      by (auto simp add: step.simps)
 
-  with `initialState program ~~ tr \<leadsto>* S'`
-  have "initialState program ~~ tr@[(i, AInvcheck False)] \<leadsto>* S'"
-    using steps_step by blast
+    with `initialState program ~~ tr \<leadsto>* S'`
+    have "initialState program ~~ tr@[(i, AInvcheck False)] \<leadsto>* S'"
+      using steps_step by blast
 
-  have "\<not>traceCorrect (tr@[(i, AInvcheck False)])"
-    by (auto simp add: traceCorrect_def)
+    have "\<not>traceCorrect (tr@[(i, AInvcheck False)])"
+      by (simp add: actionCorrect_def traceCorrect_def)
 
-  with `programCorrect program` and `initialState program ~~ tr@[(i, AInvcheck False)] \<leadsto>* S'`
-  show False
-    by (auto simp add: programCorrect_def traces_def)
+    with `programCorrect program` and `initialState program ~~ tr@[(i, AInvcheck False)] \<leadsto>* S'`
+    show False
+      by (auto simp add: programCorrect_def traces_def)
+  next 
+    assume "a = ALocal False"
+
+    hence "\<not>traceCorrect tr"
+      using \<open>(i, a) \<in> set tr\<close> actionCorrect_def traceCorrect_def by fastforce
+
+    with `programCorrect program` and `initialState program ~~ tr \<leadsto>* S'`
+    show False
+      by (auto simp add: programCorrect_def traces_def)
+  qed
 qed
 
 
